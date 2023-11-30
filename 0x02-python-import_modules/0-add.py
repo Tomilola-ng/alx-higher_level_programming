ef182de95c6ad3762591bc94f41d35aa7dccff1a
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-11-30 14:50:54 +0000

    PYTHONZ

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
-import add_0
-answer = add_0.add(1, 2)
-print(f"1 + 2 = {answer}")
+from add_0 import add
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+
+    result = add(a, b)
+
+    print("{:d} + {:d} = {:d}".format(a, b, result))
