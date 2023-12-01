180014ceec47b58f12ee4da2a818276094f71123
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-12-01 00:37:06 +0000

    CALCULATE

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, mul, div, sub
-
 if __name__ == "__main__":
+    from calculator_1 import sub, div, mul, add
+
     a = 10
     b = 5
+
+    mul_answer = mul(a, b)
+    add_answer = add(a, b)
+    div_answer = div(a, b)
+    sub_answer = sub(a, b)
+
+    print("{:d} + {:d} = {:d}".format(a, b, add_answer))
+    print("{:d} - {:d} = {:d}".format(a, b, sub_answer))
+    print("{:d} * {:d} = {:d}".format(a, b, mul_answer))
+    print("{:d} / {:d} = {:d}".format(a, b, div_answer))
