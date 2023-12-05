f4af725a40610e9bd06b1a68c81adb66386b3cbd
Tomilola-ng <tee.o2809@gmail.com>
2023-12-04 21:01:55 -0800

    f

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,10 @@
 #6-print_matrix_integer.py
 def print_matrix_integer(matrix=[[]]):
     """ Arrays of Arrays """
+    if not matrix:
+        print()
     for _arr in matrix:
-        print(" ".join("{:d}".format(i) for i in _arr))
+        print(_arr)
+        # print(" ".join("{:d}".format(_arr)))
+        # for i in _arr:
+        # print(" ".join("{:d}".format(i) for i in _arr))
