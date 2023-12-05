61915a3175e8d02638663a6e102844ec33bebbce
Tomilola-ng <tee.o2809@gmail.com>
2023-12-04 19:59:07 -0800

    LIST_PRINTER

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def print_list_integer(list_item):
+    for j in list_item:
+        print(j,"\n")
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,2 @@
+# 0x03-python-data_structures
+## ALX Higher Level Programming
