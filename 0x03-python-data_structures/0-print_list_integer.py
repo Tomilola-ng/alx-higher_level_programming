0f7413efa02dbd54c9fe379b5fd021afddee21e5
Tomilola-ng <tee.o2809@gmail.com>
2023-12-04 20:03:58 -0800

    MUCH_BETTER

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+# 0-print_list_integer.py
 def print_list_integer(list_item):
+    """print all integers in list_item"""
     for j in list_item:
-        print(j,"\n")
+        print('{:d}'.format(j))
