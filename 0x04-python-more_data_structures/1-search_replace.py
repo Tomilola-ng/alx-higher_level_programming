0ffbba9d04de6aa3307bebe6c932224c007a6c80
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-12-09 01:00:45 +0100

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
 def search_replace(my_list, search, replace):
   my_new_list = []
   for item in my_list:
     if item == search:
       my_new_list.append(replace)
-    my_new_list.append(item)
-  return
+    else:
+        my_new_list.append(item)
+  return my_new_list
