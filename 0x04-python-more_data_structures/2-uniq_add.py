9f7a42ce97bcc6c95a9ab9ff827f3718350e382c
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-12-09 01:09:48 +0100

    Uniquely Added

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    used = []
+    for i in my_list:
+        if i not in used:
+            result += i
+        used.append(i)
+    return result
