65330a9ebf7427c2cee962d05188def3de1c4029
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2024-01-08 06:24:26 +0100

    MAX

--- 0x07-python_test_driven_development/6-max_integer.py
+++ 0x07-python_test_driven_development/6-max_integer.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
 """
-Module to find the max integer in a list
+    BIGGEST INT IN A PARSED LIST
 """
 
 
 def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
     """
+    Find and return the maximum integer in a list of integers.
+
+    If the list is empty, the function returns None.
+
+    Args:
+    - lst (list): List of integers.
+
+    Returns:
+    - int or None: The maximum integer in the list or None if the list is empty.
+    """
+
     if len(list) == 0:
         return None
-    result = list[0]
+
+    max = list[0]
     i = 1
+
     while i < len(list):
-        if list[i] > result:
-            result = list[i]
+        if list[i] > max:
+            max = list[i]
         i += 1
-    return result
+
+    return max
