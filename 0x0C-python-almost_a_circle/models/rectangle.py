707faa25d8301a05f9cbd83c95358fc43259210f
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2024-01-16 04:54:46 +0000

    RECTANGLE_FIX

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,7 +24,7 @@ class Rectangle(Base):
     def width(self, value):
         """ property Width setter """
 
-        if isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -40,7 +40,7 @@ class Rectangle(Base):
     def height(self, value):
         """ property height setter """
 
-        if isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -56,7 +56,7 @@ class Rectangle(Base):
     def x(self, value):
         """ property position y setter """
 
-        if isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -72,7 +72,7 @@ class Rectangle(Base):
     def y(self, value):
         """ property y setter """
 
-        if isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
