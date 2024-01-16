ccc63a009e429800815ce4332d14d1eba90b9908
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2024-01-16 04:45:58 +0000

    RECTANGLE_SETUP

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ class Base:
 
     def __init__(self, id=None):
         """ Instantiate data """
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
