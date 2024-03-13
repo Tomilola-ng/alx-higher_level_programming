c8f5a5c8a71ef18396452fd330fc4b17930e7f8a
Tomilola-ng <tee.o2809@gmail.com>
2024-03-12 19:08:58 -0700

    6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const allStrings = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < allStrings.length; i++) {
+  console.log(allStrings[i]);
+}
