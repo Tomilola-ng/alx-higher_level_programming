07a1cb13c1aab237022e2e883d0f7ee9e9fa89b4
Tomilola-ng <tee.o2809@gmail.com>
2024-03-12 18:41:13 -0700

    2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 
 const { argv } = require('node:process');
 
-if (argv.length === 0) {
+if (argv.length === 2) {
   console.log('No argument');
-} else if (argv.length === 1) {
+} else if (argv.length === 3) {
   console.log('Argument found');
-} else {
+} else if (argv.length > 3) {
   console.log('Arguments found');
 }
