8e7ee0ba38a89aa4440a4404154dc0393bca0e18
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2024-04-25 01:05:27 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 
 const request = require("request");
-const url = process.argv[2];
+const movieId = process.argv[2];
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(body).title);
+request(
+  `https://swapi-api.alx-tools.com/api/films/${movieId}`,
+  function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).title);
+    }
   }
-});
+);
